18613923f98edbb581ff1b477afd41daba3334d1
tesfahunfola <t.zanafola@alustudent.com>
2024-04-07 16:18:57 +0200

    update

---
+++ assets/js/script.js
@@ -0,0 +1,72 @@
+"use strict";
+
+/**
+ * navbar toggle
+ */
+
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+
+const navElemArr = [navOpenBtn, navCloseBtn];
+
+for (let i = 0; i < navElemArr.length; i++) {
+  navElemArr[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+  });
+}
+
+/**
+ * toggle navbar when click any navbar link
+ */
+
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.remove("active");
+  });
+}
+
+/**
+ * header active when window scrolled down
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 50
+    ? header.classList.add("active")
+    : header.classList.remove("active");
+});
+
+// Login Script
+function myMenuFunction() {
+  var i = document.getElementById("navMenu");
+  if (i.className === "nav-menu") {
+    i.className += " responsive";
+  } else {
+    i.className = "nav-menu";
+  }
+}
+
+var a = document.getElementById("loginBtn");
+var b = document.getElementById("registerBtn");
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+function login() {
+  x.style.left = "4px";
+  y.style.right = "-520px";
+  a.className += " white-btn";
+  b.className = "btn";
+  x.style.opacity = 1;
+  y.style.opacity = 0;
+}
+function register() {
+  x.style.left = "-510px";
+  y.style.right = "5px";
+  a.className = "btn";
+  b.className += " white-btn";
+  x.style.opacity = 0;
+  y.style.opacity = 1;
+}
